9d2f24c94a13a3e63f39c217eb70a50e359f5932
Brenden Bass <80184025+brendenbass@users.noreply.github.com>
2022-05-25 11:26:25 -0400

    Finished the game!

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Threading;
 
 namespace Hangman
@@ -50,7 +51,10 @@ namespace Hangman
  | |
  |_|" };
 
-            string startWord = "guess the word";
+            string startWord = "BRENDEN";
+            string maskStartWord = new string('-', startWord.Length);
+            string guessedCharacter = "";
+            string guessedCharacterList = "";
 
             bool gameOver = false;
 
@@ -67,6 +71,51 @@ namespace Hangman
                 Console.WriteLine(messages[0]);
                 Console.WriteLine(item);
                 Thread.Sleep(1000);
+            }
+
+            //Game Logic
+            while (!gameOver)
+            {
+                Console.Clear();
+                Console.WriteLine("Guess the word: {0}", maskStartWord);
+                Console.WriteLine("Guessed Characters: {0}", guessedCharacterList);
+                Console.WriteLine("You have {0} guesses left.", guessTries);
+                Console.WriteLine();
+                Console.Write("Your next guess is: ");    
+                guessedCharacter = Console.ReadLine().ToUpper();
+                guessedCharacterList += guessedCharacter + " ";
+
+                if (startWord.Contains(guessedCharacter))
+                {
+                    for (int i = 0; i <= startWord.Length - 1; i++)
+                    {
+                        string currentCharacter = startWord[i].ToString();
+                        if (currentCharacter == guessedCharacter)
+                        {
+                            char[] maskedArray = maskStartWord.ToCharArray();
+                            maskedArray[i] = Convert.ToChar(guessedCharacter);
+                            maskStartWord = new string(maskedArray);
+                        }
+                    }
+                    if (startWord == maskStartWord)
+                    {
+                        Console.Clear();
+                        Console.WriteLine(messages[1]);
+                        Console.WriteLine("Congratulations, you guessed the word: {0}!", startWord);
+                        gameOver = true;
+                    }
+                }
+                else
+                {
+                    guessTries--;
+                }
+
+                if (guessTries == 0)
+                {
+                    gameOver = true;
+                    Console.WriteLine(messages[2]);
+                    Console.WriteLine("The word was {0)", startWord);
+                }
             }
         }
     }
